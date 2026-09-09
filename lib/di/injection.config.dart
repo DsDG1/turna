3c9bedb936c8f6cf3f80b5eae3543a6e62ebf327
dsdogs <dsdogs@outlook.com>
2026-09-09 00:17:49 +0000

    fix(ci): comment out hardcoded Windows JDK path in gradle.properties and strip in CI

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -9,6 +9,7 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+
 import 'package:audioplayers/audioplayers.dart' as _i656;
 import 'package:flutter_tts/flutter_tts.dart' as _i50;
 import 'package:get_it/get_it.dart' as _i174;
@@ -157,8 +158,6 @@ extension GetItInjectableX on _i174.GetIt {
       () => audioModule.speechPlayer,
       instanceName: 'speechPlayer',
     );
-    gh.lazySingleton<_i551.MistakeProvider>(
-        () => _i551.MistakeProvider(gh<_i523.AppPrefs>()));
     gh.lazySingleton<_i656.AudioPlayer>(
       () => audioModule.audioPlayer,
       instanceName: 'audioPlayer',
@@ -181,6 +180,8 @@ extension GetItInjectableX on _i174.GetIt {
         () => _i854.LessonLinkStore(gh<_i523.AppPrefs>()));
     gh.lazySingleton<_i409.LessonProgressProvider>(
         () => _i409.LessonProgressProvider(gh<_i523.AppPrefs>()));
+    gh.lazySingleton<_i551.MistakeProvider>(
+        () => _i551.MistakeProvider(gh<_i523.AppPrefs>()));
     gh.lazySingleton<_i166.ScoreProvider>(
         () => _i166.ScoreProvider(gh<_i523.AppPrefs>()));
     gh.lazySingleton<_i927.StreakProvider>(
--- lib/routing/routing.gr.dart
+++ lib/routing/routing.gr.dart
@@ -9,6 +9,7 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+
 import 'dart:async' as _i69;
 
 import 'package:auto_route/auto_route.dart' as _i54;
